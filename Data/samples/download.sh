31a7fab03ff043fd19445d4ac019530b9dd3c306
geparada <gp7@sanger.ac.uk>
2021-01-12 05:37:59 +0000

    data folder

---
+++ Data/samples/download.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+fastq-dump SRR363927 --gzip
+fastq-dump SRR363931 --gzip
+fastq-dump SRR363932 --gzip
+fastq-dump SRR363935 --gzip
+fastq-dump SRR363938 --gzip
+fastq-dump SRR363940 --gzip
